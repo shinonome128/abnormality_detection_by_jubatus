e2a32f0321561f687c6dea70cd57b3200b97f3d2
shinonome128 <shinonome128@gmail.com>
2018-12-05 16:35:23 +0900

    Change credentials and project

--- terraform/gcp_provider.tf
+++ terraform/gcp_provider.tf
@@ -1,7 +1,7 @@
 # gcp_provider.tf
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("C:/Users/shino/doc/cicddemo/cicd-demo-707b32bf1a7f.json")}"
-  project     = "cicd-demo-215605"
+  credentials = "${file("C:/Users/shino/doc/abnormality_detection_by_jubatus/jubatus  -d418843cdf68.json")}"
+  project     = "jubatus-224508"
   region      = "asia-northeast1"
 }
