321a4ddd6b68b906788d4ea614968d59e374035f
shinonome128 <shinonome128@gmail.com>
2018-12-05 16:40:30 +0900

    Change network name

--- terraform/gcp_network.tf
+++ terraform/gcp_network.tf
@@ -1,12 +1,12 @@
 # gcp_network.tf 
 
-resource "google_compute_network" "cicddemo" {
-  name = "cicddemo"
+resource "google_compute_network" "jubatus" {
+  name = "jubatus"
 }
 resource "google_compute_subnetwork" "development" {
   name          = "development"
   ip_cidr_range = "10.30.0.0/16"
-  network       = "${google_compute_network.cicddemo.name}"
+  network       = "${google_compute_network.jubatus.name}"
   description   = "development"
   region        = "asia-northeast1"
 }
